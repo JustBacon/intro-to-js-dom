c08509788fb1b365a47a9f199d31260c9abed6e0
Christian Buco <christianbuco17@gmail.com>
2017-03-13 08:33:34 -1000

    Get class name

--- js/app.js
+++ js/app.js
@@ -3,5 +3,13 @@ console.log("hi");
 // console.log(document.getElementById("use-id-method"));
 var idElement = document.getElementById("use-id-method")
 console.log(idElement);
-console.log(document.getElementsByTagName("script"))
 
+
+// console.log(document.getElementsByTagName("script"))
+var tagName = document.getElementsByTagName("script")
+console.log(tagName)
+
+
+// console.log(document.getElementsByClassName("use-class-method")[0])
+var classElement = document.getElementsByClassName("use-class-method")[0]
+console.log(classElement)
