b622a8e2c8f3ee62f21516ba8a53836cf1102183
Christian Buco <christianbuco17@gmail.com>
2017-03-13 08:28:20 -1000

    get tag name

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log("hi")
+console.log("hi");
+//Finding HTML Elements
+// console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method")
+console.log(idElement);
+console.log(document.getElementsByTagName("script"))
+
